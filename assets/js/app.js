3673dc51eda684afbdf95fdb3ef13bb6ccf6a873
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-19 09:46:55 -1200

    start time left

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,11 +42,11 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML = days + ":" + hours + ":"
+  document.getElementById("number").innerHTML = days + ":" + hours + ":"
     + minutes + ":" + seconds;
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("number").innerHTML = "EXPIRED";
   }
 }, 1000);
