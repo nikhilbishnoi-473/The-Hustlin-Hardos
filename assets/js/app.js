0fefdac96a7a22efc114538747241e91799f3600
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-19 09:42:18 -1200

    change header

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,26 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+var countDownDate = new Date("Apr 25, 2024 15:37:25").getTime();
+
+var x = setInterval(function () {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + ":" + hours + ":"
+    + minutes + ":" + seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
